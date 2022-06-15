ec859ae4e2d9460abc49880cf97edc416b943797
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-15 12:27:48 +0100

    Render the components using home component

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import NavBar from './NavBar';
+// import NavBar from './NavBar';
 
 const Container = ({ children, className = '' }) => (
   <>
-    <NavBar />
+    {/* <NavBar /> */}
     <div className={`px-4 py-2 mx-auto max-w-7x1 sm:px-6 lg:px-8 ${className}`}>
       <div className="max-w-3x1 mx-auto">{children}</div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import Button from './Button';
+import Container from './Container';
+import Input from './Input';
+import Table from './Tasks/Table';
 
 const Home = () => {
   const name = 'John';
+  const data = [
+    { id: 1, title: 'Task 1', description: 'Task 1 description' },
+    { id: 2, title: 'Task 2', description: 'Task 2 description' },
+    { id: 3, title: 'Task 3', description: 'Task 3 description' },
+  ];
   return (
     <div>
-      <h1>Home</h1>
-      {name}
-      <Button
-        buttonText="testing"
-        className="h-10 px-6 font-semibold rounded-md bg-sky-500 text-white"
-        type="button"
-      />
+      <Container className="container">
+        <h1>Home</h1>
+        {name}
+        <Button
+          buttonText="testing"
+          className="h-10 px-6 font-semibold rounded-md bg-sky-500 text-white"
+          type="button"
+          onClick={() => console.log('hello')}
+          loading={false}
+        />
+        <Input type="text" placeholder="Enter Email" />
+        <Table data={data} />
+      </Container>
     </div>
   );
 };
