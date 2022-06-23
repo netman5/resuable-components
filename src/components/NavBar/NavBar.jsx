9616c1b0f1cc156aeff0593d3597a266c7e351ca
Ola Ishola <olaishola@hotmail.co.uk>
2022-06-23 21:35:46 +0100

    Add styling to nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,29 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 import Menu from '../Icons/Icons';
+import styles from './NavBar.module.css';
 
 const NavBar = () => {
   const user = 'loko';
   console.log(user);
 
   return (
-    <nav>
-      <a href="/">TestCom</a>
-      <Menu />
+    <nav className={styles.navigation}>
+      <a href="/" className={styles.brandName}>TestCom</a>
+      <button type="button" className={styles.hamburger}><Menu /></button>
+      <div className={styles.navigationMenu}>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
